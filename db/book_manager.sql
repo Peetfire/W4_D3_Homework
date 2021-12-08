2bc8890381c8916e35f898975c60c3ea5c555541
petermordaunt <peetfire@hotmail.com>
2021-12-08 15:18:11 +0000

    Diagrams & Wireframes, created classes, set up tables & DB

--- db/book_manager.sql
+++ db/book_manager.sql
@@ -2,9 +2,15 @@ DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE authors (
-  
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR
 );
 
 CREATE TABLE books (
-  
+    id SERIAL PRIMARY KEY,
+    title VARCHAR,
+    genre VARCHAR,
+    publisher VARCHAR,
+    author_id INT REFERENCES authors(id)
 );
--- models/book.py
+++ models/book.py
@@ -1,4 +1,8 @@
 class Book:
 
-    def __init__( ):    
-        pass
+    def __init__(self, title, genre, author, publisher, id = None):
+        self.title = title
+        self.genre = genre
+        self.author = author
+        self.publisher = publisher
+        self.id = id
